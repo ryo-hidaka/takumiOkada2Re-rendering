cc2f42e37679c6ca8429c290ca2b44f249ec20f6
ryo-hidaka <66785120+ryo-hidaka@users.noreply.github.com>
2022-05-18 11:17:59 +0900

    再レンダリング

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import "./styles.css";
+import { ChildArea } from "./ChildArea";
 
 export default function App() {
+  console.log("App");
+  const [text, setText] = useState("");
+  const [opne, setOpen] = useState(false);
+
+  const onChangeText = (event) => {
+    setText(event.target.value);
+  };
+
+  const onClickOpne = () => {
+    setOpen(!opne);
+  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpne}>表示</button>
+      <ChildArea open={opne} />
     </div>
   );
 }
